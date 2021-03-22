ca0b22790f23cae1ffa7706d8fcc04ab1bc5b931
Compiler <LukeRocheDev@gmail.com>
2021-03-22 13:26:27 -0400

    finished posix semaphores

--- LNDriver/src/main.c
+++ LNDriver/src/main.c
@@ -7,8 +7,6 @@ int main(int argc, char* argv[]){
 
     printf("Starting main driver\n");
     DBG_WRAP_DRIVER(psemaphore_execute(argc, argv));
-    DBG_WRAP_DRIVER(message_queue_execute(argc, argv));
-    DBG_WRAP_DRIVER(shared_mem_execute(argc, argv));
     return 0;
 
 }
