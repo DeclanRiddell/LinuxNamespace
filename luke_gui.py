f6c049f6cf77e29b5d834e4594797587ed49eae7
compiler <lukerochedev@gmail.com>
2021-04-27 18:19:22 -0400

    buttons 6/12 centerd

--- luke_gui.py
+++ luke_gui.py
@@ -4,6 +4,31 @@ c_width = 1080; c_height = 480;
 root = Tk(className="Linux IPC Metrics")
 canvas = Canvas(root, width = c_width, height = c_height)
 canvas.pack()
+my_map = {"native" : 
+                    {"sysv" :  
+                            {"messagequeue" : IntVar(), 
+                             "sharedmemory" : IntVar(), 
+                             "semaphore" : IntVar()}, 
+
+                    "posix" : {"messagequeue" : IntVar(), 
+                               "sharedmemory" : IntVar(), 
+                               "semaphore" : IntVar()
+                               }
+                    }, 
+          "namespace" : 
+                    {"sysv" :  {"messagequeue" : IntVar(), 
+                                "sharedmemory" : IntVar(), 
+                                "semaphore" : IntVar()
+                                }, 
+
+                    "posix" :  {"messagequeue" : IntVar(), 
+                                "sharedmemory" : IntVar(), 
+                                "semaphore" : IntVar()
+                                }
+                    } 
+        }
+
+
 
 
 def method2():
@@ -26,14 +51,14 @@ def change_size_callback(event):
 p_col = '#ccccff'
 font_type= 'Arial'
 font_size = 12
+picker_frame = Frame(root, bg=p_col)
+matplot_frame = Frame(root, bg="#e6ffff")
 def init():
     print(canvas['width'])
 
     root.bind('<Configure>',change_size_callback)
     picker_w = 0.5; picker_h = 1;
-    picker_frame = Frame(root, bg=p_col)
     picker_frame.place(width = c_width * picker_w, relheight=picker_h)
-    matplot_frame = Frame(root, bg="#e6ffff")
     matplot_frame.place(relwidth = 1.0 , relheight = picker_h, x=c_width * picker_w, )
     root.update()
 
@@ -41,7 +66,7 @@ def init():
 
    
     #run_button = Button(picker_frame, text="Run", padx=picker_frame.winfo_width() / 2, pady=5, anchor='c').grid(row=0,column=0,rowspan=10);
-    Button(picker_frame, text="Run", anchor='c', padx=15, bg='#ccffe6').grid(row=0,column=0, columnspan=1);
+    #Button(picker_frame, text="Run", anchor='c', padx=15, bg='#ccffe6').grid(row=10,column=0, columnspan=1);
     #Label(picker_frame, text="Namespace", bg=p_col).grid(row=0, column=0)
     offset = 2
     namespace_label = Label(picker_frame, text="Namespace",font=(font_type, font_size), bg=p_col).grid(row=0+(int)(offset/2), column=2)
@@ -56,7 +81,7 @@ def init():
     blank_label2 = Label(picker_frame, text="\t", bg=p_col,font=(font_type, font_size)).grid(row=offset, column=0)
     semaphore_label = Label(picker_frame, text="Semaphores", bg=p_col, justify=LEFT, anchor='w',font=(font_type, font_size)).grid(row=2+offset, column=0, sticky='w')
     Label(picker_frame, text="\t", bg=p_col,font=(font_type, font_size)).grid(row=3+offset, column=0)
-    message_queue_label = Label(picker_frame, text="Me Queue", bg=p_col,font=(font_type, font_size)).grid(row=4+offset, column=0, sticky='w')
+    message_queue_label = Label(picker_frame, text="Message Queue", bg=p_col,font=(font_type, font_size)).grid(row=4+offset, column=0, sticky='w')
     Label(picker_frame, text="\t", bg=p_col,font=(font_type, font_size)).grid(row=5+offset, column=0)
     shared_memory_label = Label(picker_frame, text="Shared Memory", bg=p_col,font=(font_type, font_size)).grid(row=6+offset, column=0, sticky='w')
 
@@ -70,7 +95,7 @@ def init():
     tkinter.ttk.Separator(picker_frame, orient=HORIZONTAL).grid(column=1, row=5, columnspan=8, sticky='ew') 
     tkinter.ttk.Separator(picker_frame, orient=HORIZONTAL).grid(column=1, row=7, columnspan=8, sticky='ew') 
     tkinter.ttk.Separator(picker_frame, orient=HORIZONTAL).grid(column=1, row=9, columnspan=8, sticky='ew') 
-
+    setup_map_checkboxes()
     
     # ipc_label.grid(row=0, column=0)
     # semaphore_label.grid(row=2, column=0)
@@ -82,6 +107,66 @@ def init():
     # sysv_label.grid(row=1, column=7)
     root.mainloop()
 
+
+def setup_map_checkboxes():
+    native_posix_semaphore_button = Checkbutton(picker_frame,  var = my_map['native']['posix']['semaphore'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 4, column = 5)
+    native_posix_messagequeue_button = Checkbutton(picker_frame,  var = my_map['native']['posix']['messagequeue'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 6, column = 5)
+    native_posix_sharedmem_button = Checkbutton(picker_frame,  var = my_map['native']['posix']['sharedmemory'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 8, column = 5)
+    native_sysv_semaphore_button = Checkbutton(picker_frame,  var = my_map['native']['sysv']['semaphore'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 4, column = 7)
+    native_sysv_messagequeue_button = Checkbutton(picker_frame,  var = my_map['native']['sysv']['messagequeue'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 6, column = 7)
+    native_sysv_sharedmem_button = Checkbutton(picker_frame,  var = my_map['native']['sysv']['sharedmemory'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 8, column = 7)
+
+    namespace_posix_semaphore_button = Checkbutton(picker_frame,  var = my_map['namespace']['posix']['semaphore'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 4, column = 1)
+    namespace_posix_messagequeue_button = Checkbutton(picker_frame, var = my_map['namespace']['posix']['messagequeue'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 6, column = 1)
+    namespace_posix_sharedmem_button = Checkbutton(picker_frame, var = my_map['namespace']['posix']['sharedmemory'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 8, column = 1)
+    namespace_sysv_semaphore_button = Checkbutton(picker_frame, var = my_map['namespace']['sysv']['semaphore'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 4, column = 3)
+    namespace_sysv_messagequeue_button = Checkbutton(picker_frame, var = my_map['namespace']['sysv']['messagequeue'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 6, column = 3)
+    namespace_sysv_sharedmem_button = Checkbutton(picker_frame, var = my_map['namespace']['sysv']['sharedmemory'], bg=p_col, onvalue = 1, offvalue = 0).grid(row = 8, column = 3)
+
+
+    #Native POSIX IPCs
+    if(my_map['native']['posix']['semaphore'].get()):
+        print("native posix semaphore")
+        subprocess.run(['sudo make eric'], shell = True)    
+    if(my_map['native']['posix']['messagequeue'].get()):
+        print("native posix message queue")    
+        subprocess.run(['sudo make alex'], shell = True)    
+    if(my_map['native']['posix']['sharedmemory'].get()):
+        print("native posix shared memory")    
+        subprocess.run(['sudo make vincent'], shell = True)    
+
+#Namespace POSIX IPCs
+    if(my_map['namespace']['posix']['semaphore'].get()):
+        print("namespace posix semaphore")    
+        subprocess.run(['sudo make eric_namespace'], shell = True)    
+    if(my_map['namespace']['posix']['messagequeue'].get()):
+        print("namespace posix message queue")    
+        subprocess.run(['sudo make alex_namespace'], shell = True)    
+    if(my_map['namespace']['posix']['sharedmemory'].get()):
+        print("namespace posix shared memory")    
+        subprocess.run(['sudo make vincent_namespace'], shell = True)    
+
+#Native SYSV IPCs
+    if(my_map['native']['sysv']['semaphore'].get()):
+        print("native sysv semaphore")    
+        subprocess.run(['sudo make eric'], shell = True)    
+    if(my_map['native']['sysv']['messagequeue'].get()):
+        print("native sysv message queue")    
+        subprocess.run(['sudo make alex'], shell = True)    
+    if(my_map['native']['sysv']['sharedmemory'].get()):
+        print("native sysv shared memory")  
+        subprocess.run(['sudo make vincent'], shell = True)    
+  
+#Namespace SYSV IPCs
+    if(my_map['namespace']['sysv']['semaphore'].get()):
+        print("namespace sysv semaphore")    
+        subprocess.run(['sudo make eric_namespace'], shell = True)    
+    if(my_map['namespace']['sysv']['messagequeue'].get()):
+        print("namespace sysv message queue")    
+        subprocess.run(['sudo make alex_namespace'], shell = True)    
+    if(my_map['namespace']['sysv']['sharedmemory'].get()):
+        print("namespace sysv shared memory")   
+        subprocess.run(['sudo make vincent_namespace'], shell = True)
 
 if __name__ == '__main__':
     init()
