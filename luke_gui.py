cbfe8acd3f1cc1c7f1e2397c3cb7852fb772bfc4
compiler <lukerochedev@gmail.com>
2021-04-27 08:25:14 -0400

    some of the layout

--- luke_gui.py
+++ luke_gui.py
@@ -1,31 +1,68 @@
 from tkinter import *
-
-c_width = 720; c_height = 480;
+import tkinter.ttk
+c_width = 1080; c_height = 480;
 root = Tk(className="Linux IPC Metrics")
 canvas = Canvas(root, width = c_width, height = c_height)
 canvas.pack()
+
+
+def method2():
+    distro_label = Label(picker_frame, text="Namespace\t\tNative", bg=p_col).pack()
+    namespace_lib_label = Label(picker_frame, text="POSIX\t\tSysv\tPOSIX\t\tSysv", bg=p_col).pack()
+   
+   
+    semaphore_label = Label(picker_frame, text="Semaphores", bg=p_col, justify=LEFT, anchor='w')
+    message_queue_label = Label(picker_frame, text="Message Queue", bg=p_col).pack()
+    shared_memory_label = Label(picker_frame, text="Shared Memory", bg=p_col).pack()
+    posix_label = Label(picker_frame, text="Posix", bg=p_col).pack()
+    sysv_label = Label(picker_frame, text="SysV", bg=p_col).pack()
+    run_button = Button(picker_frame, text="Run", padx=picker_frame.winfo_width() / 2, pady=5, anchor='c').grid(row=0,column=0,rowspan=10);
 
 def change_size_callback(event):
     print(event.width,event.height)
     c_width = event.width;
     c_height = event.height
+
+p_col = '#ccccff'
+font_type= 'Arial'
+font_size = 12
 def init():
     print(canvas['width'])
 
     root.bind('<Configure>',change_size_callback)
+    picker_w = 0.5; picker_h = 1;
+    picker_frame = Frame(root, bg=p_col)
+    picker_frame.place(width = c_width * picker_w, relheight=picker_h)
+    matplot_frame = Frame(root, bg="#e6ffff")
+    matplot_frame.place(relwidth = 1.0 , relheight = picker_h, x=c_width * picker_w, )
+    root.update()
 
-    picker_w = 0.4; picker_h = 1;
-    picker_frame = Frame(root, bg="#4F4776")
-    picker_frame.place(width = 720 * picker_w, relheight=picker_h)
 
-    matplot_frame = Frame(root, bg="#475E76")
-    matplot_frame.place(relwidth = 1.0 , relheight = picker_h, x=720 * picker_w, )
-    ipc_label = Label(picker_frame, text="IPC\tSup").pack()
-    semaphore_label = Label(picker_frame, text="Semaphores").pack()
-    message_queue_label = Label(picker_frame, text="Message Queue").pack()
-    shared_memory_label = Label(picker_frame, text="Shared Memory").pack()
-    posix_label = Label(picker_frame, text="Posix").pack()
-    sysv_label = Label(picker_frame, text="SysV").pack()
+
+   
+    #run_button = Button(picker_frame, text="Run", padx=picker_frame.winfo_width() / 2, pady=5, anchor='c').grid(row=0,column=0,rowspan=10);
+    Button(picker_frame, text="Run", anchor='c', padx=15, bg='#ccffe6').grid(row=0,column=0, columnspan=1);
+    #Label(picker_frame, text="Namespace", bg=p_col).grid(row=0, column=0)
+    offset = 2
+    namespace_label = Label(picker_frame, text="Namespace",font=(font_type, font_size), bg=p_col).grid(row=0+(int)(offset/2), column=2)
+    native_label = Label(picker_frame, text="Native",font=(font_type, font_size), bg=p_col).grid(row=0+(int)(offset/2), column=6)
+    blank_label = Label(picker_frame, text="\t", bg=p_col,font=(font_type, font_size)).grid(row=0+(int)(offset/2), column=4)
+    namespace_posix_label = Label(picker_frame, text="POSIX", bg=p_col,font=(font_type, font_size)).grid(row=1+(int)(offset/2), column=1)
+    namespace_sysv_label = Label(picker_frame, text="SysV", bg=p_col,font=(font_type, font_size)).grid(row=1+(int)(offset/2), column=3)
+    native_posix_label = Label(picker_frame, text="POSIX", bg=p_col,font=(font_type, font_size)).grid(row=1+(int)(offset/2), column=5)
+    native_sysv_label = Label(picker_frame, text="SysV", bg=p_col,font=(font_type, font_size)).grid(row=1+(int)(offset/2), column=7)
+   
+   
+    blank_label2 = Label(picker_frame, text="\t", bg=p_col,font=(font_type, font_size)).grid(row=offset, column=0)
+    semaphore_label = Label(picker_frame, text="Semaphores", bg=p_col, justify=LEFT, anchor='w',font=(font_type, font_size)).grid(row=2+offset, column=0, sticky='w')
+    Label(picker_frame, text="\t", bg=p_col,font=(font_type, font_size)).grid(row=3+offset, column=0)
+    message_queue_label = Label(picker_frame, text="Message Queue", bg=p_col,font=(font_type, font_size)).grid(row=3+offset, column=0, sticky='w')
+    shared_memory_label = Label(picker_frame, text="Shared Memory", bg=p_col,font=(font_type, font_size)).grid(row=4+offset, column=0, sticky='w')
+    posix_label = Label(picker_frame, text="Posix", bg=p_col,font=(font_type, font_size)).grid(row=5+offset, column=0, sticky='w')
+    sysv_label = Label(picker_frame, text="SysV", bg=p_col,font=(font_type, font_size)).grid(row=6+offset, column=0, sticky='w')
+
+    tkinter.ttk.Separator(picker_frame, orient=VERTICAL).grid(column=0, row=3, rowspan=8, sticky='ens') 
+    tkinter.ttk.Separator(picker_frame, orient=VERTICAL).grid(column=2, row=3, rowspan=8, sticky='ns') 
 
 
     
