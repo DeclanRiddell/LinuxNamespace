200c89c8135c1f40299fabbe0457074812241897
compiler <lukerochedev@gmail.com>
2021-04-30 13:10:00 -0400

    WORKING VERSION LMAOOOOOO

--- luke_gui.py
+++ luke_gui.py
@@ -11,7 +11,8 @@ import pandas as pd
 import csv
 c_width = 1120; c_height = 1000;
 root = Tk(className="- ASRC Linux Namespace Performance Project -")
-canvas = Canvas(root, width = c_width, height = c_height)
+root.resizable(False, False)
+canvas = Canvas(root, width = c_width, height = c_height, bg='skyblue')
 canvas.pack()
 iteration_count = 444;
 
@@ -146,37 +147,32 @@ def create_table():
     plt.clf()
 
 def create_graph():
-    fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1])
-    types = ['POSIX Semaphore','SysV Semaphore','POSIX Shared Memory','SysV Shared Memory', 'POSIX Message queue']
-    times = [random.uniform(0,1), random.uniform(0,1), random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)]
-    ax.bar(types, times)
-    ax.set_xlabel = "IPCS"
-    ax.set_ylabel = "Time"
-    plt.savefig('Resources/bar_graph.png')#, dpi = 400)
-
-
-    sns.set(style = "ticks", color_codes = True)
-
-    ipc_data = pd.read_csv("data.csv")
-    fig = sns.catplot(x = "AVG_TIME", y = "IPC", data = ipc_data, kind = "strip", hue = "ENVIRONMENT", dodge=True)
-    plt.savefig('test_pic.png', dpi = 100)
-    #plt.show()
-    plt.savefig('Resources/xd_graph.png')
-    plt.cla()
-    plt.clf()
-    plt.close()
-    sns.set(style = "ticks", color_codes = True)
-
-    fig = sns.catplot(x = "AVG_TIME", y = "IPC", data = ipc_data, kind = "strip", hue = "LIBRARY", dodge=True)
-    plt.savefig('test_pic.png', dpi = 100)
-    #plt.show()
-    plt.savefig('Resources/bar_graph.png')
-    plt.cla()
-    plt.clf()
-    plt.close()
-
     filename = 'data.csv'
+    search = "semaphore"
+    semaphore_list = []
+    semaphore_field = ['IPC','LIBRARY','ENVIRONMENT','AVG_TIME','ITERATIONS']
+
+    with open(filename, 'r') as csvfile:
+        datareader = csv.reader(csvfile)
+        for row in datareader:
+            if search in row:
+                semaphore_list.append(row)
+
+    with open("semaphore.csv",'w') as semaphore_file:
+        csvwriter = csv.writer(semaphore_file)
+        csvwriter.writerow(semaphore_field)
+        csvwriter.writerows(semaphore_list)
+
+    semaphore_data = pd.read_csv("semaphore.csv")
+    if(semaphore_data.empty):
+        plt.draw()
+    else:
+        fig = sns.catplot(x = "AVG_TIME", y = "ENVIRONMENT" , data = semaphore_data, kind = "strip", hue = "LIBRARY", dodge = True, palette = sns.color_palette(['orange', 'blue']))
+        plt.title('Semaphore Graph')
+        fig.set_xticklabels(rotation=40, ha='right')
+        plt.tight_layout()
+        plt.savefig('Resources/semaphore_graph.png', dpi = 200)
+
     search = "message_queue"
     msgq_list = []
     msgq_field = ['IPC','LIBRARY','ENVIRONMENT','AVG_TIME','ITERATIONS']
@@ -196,29 +192,48 @@ def create_graph():
     if(msgq_data.empty):
         plt.draw()
     else:
-        fig = sns.catplot(x = "AVG_TIME", y = "ENVIRONMENT" , data = msgq_data, kind = "strip", hue = "LIBRARY", dodge = True, palette = sns.color_palette(['red', 'blue']))
+        fig = sns.catplot(x = "AVG_TIME", y = "ENVIRONMENT" , data = msgq_data, kind = "strip", hue = "LIBRARY", dodge = True, palette = sns.color_palette(['orange', 'blue']))
         plt.title('Message Queue Graph')
-    plt.savefig('Resources/msgq_graph.png', dpi = 100)
+    fig.set_xticklabels(rotation=40, ha='right')
+    plt.tight_layout()
+    plt.savefig('Resources/msgq_graph.png', dpi = 200)
+
+
+
+
+    search = "shared_memory"
+    shared_memory_list = []
+    shared_memory_field = ['IPC','LIBRARY','ENVIRONMENT','AVG_TIME','ITERATIONS']
+
+    with open(filename, 'r') as csvfile:
+        datareader = csv.reader(csvfile)
+        for row in datareader:
+            if search in row:
+                shared_memory_list.append(row)
+
+    with open("shared_memory.csv",'w') as shared_memory_file:
+        csvwriter = csv.writer(shared_memory_file)
+        csvwriter.writerow(shared_memory_field)
+        csvwriter.writerows(shared_memory_list)
+
+    shared_memory_data = pd.read_csv("shared_memory.csv")
+    if(shared_memory_data.empty):
+        plt.draw()
+    else:
+        fig = sns.catplot(x = "AVG_TIME", y = "ENVIRONMENT" , data = shared_memory_data, kind = "strip", hue = "LIBRARY", dodge = True, palette = sns.color_palette(['orange', 'blue']))
+        plt.title('Shared Memory Graph')
+        fig.set_xticklabels(rotation=40, ha='right')
+        plt.tight_layout()
+        plt.savefig('Resources/shared_memory_graph.png', dpi = 200)
     plt.cla()
     plt.clf()
 
-def method2():
-    distro_label = Label(picker_frame, text="Namespace\t\tNative", bg=p_col).pack()
-    namespace_lib_label = Label(picker_frame, text="POSIX\t\tSysv\tPOSIX\t\tSysv", bg=p_col).pack()
-   
-   
-    semaphore_label = Label(picker_frame, text="Semaphores", bg=p_col, justify=LEFT, anchor='w')
-    message_queue_label = Label(picker_frame, text="Message Queue", bg=p_col).pack()
-    shared_memory_label = Label(picker_frame, text="Shared Memory", bg=p_col).pack()
-    posix_label = Label(picker_frame, text="Posix", bg=p_col).pack()
-    sysv_label = Label(picker_frame, text="SysV", bg=p_col).pack()
-    run_button = Button(picker_frame, text="Run", padx=picker_frame.winfo_width() / 2, pady=5, anchor='c').grid(row=0,column=0,rowspan=10);
 p_col = '#ccccff'
 font_type= 'Arial'
 font_size = 12
 picker_frame = Frame(root, bg=p_col)
 matplot_frame = Frame(root, bg="#e6ffff")
-log_frame = Frame(root, bg = 'green')
+log_frame = Frame(root, bg = 'skyblue')
 log = []
 def change_size_callback(event):
     c_width = event.width
@@ -226,57 +241,52 @@ def change_size_callback(event):
 
 
 img_size = 350
-#bar_img = ImageTk.PhotoImage(Image.open('Resources/bar_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+#bar_img = ImageTk.PhotoImage(Image.open('Resources/shared_memory_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
 #hist_img = ImageTk.PhotoImage(Image.open('Resources/msgq_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-img1 = ImageTk.PhotoImage(Image.open('Resources/table.png').resize((img_size, img_size), Image.ANTIALIAS))
-img2 = ImageTk.PhotoImage(Image.open('Resources/bar_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-img3 = ImageTk.PhotoImage(Image.open('Resources/msgq_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-img4 = ImageTk.PhotoImage(Image.open('Resources/xd_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-# img2.resize((img_size, img_size), Image.ANTIALIAS)
-# img3.resize((img_size, img_size), Image.ANTIALIAS)
-# img4.resize((img_size, img_size), Image.ANTIALIAS)
-am = 10
-panel_bar = Label(matplot_frame, image = img2)
-panel_hist = Label(matplot_frame, image = img3)
-panel_xd = Label(matplot_frame, image = img4)
-panel_xd.grid(row= 0, column = 2, padx=am, pady = 50)
-panel_hist.grid(row= 0, column = 0, padx=am, pady = am)
-panel_bar.grid(row= 0, column = 1, padx=am, pady = am)
+img_tbl = ImageTk.PhotoImage(Image.open('Resources/table.png').resize((img_size, img_size), Image.ANTIALIAS))
+img_sm = ImageTk.PhotoImage(Image.open('Resources/shared_memory_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+img_mq = ImageTk.PhotoImage(Image.open('Resources/msgq_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+img_sem = ImageTk.PhotoImage(Image.open('Resources/semaphore_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+# img_sm.resize((img_size, img_size), Image.ANTIALIAS)
+# img_mq.resize((img_size, img_size), Image.ANTIALIAS)
+# img_sem.resize((img_size, img_size), Image.ANTIALIAS)
+padding = [10, 100]
+panel_sm = Label(matplot_frame, image = img_sm)
+panel_mq = Label(matplot_frame, image = img_mq)
+panel_sem = Label(matplot_frame, image = img_sem)
+panel_sem.grid(row= 0, column = 2, padx=padding[0], pady = padding[1])
+panel_mq.grid(row= 0, column = 0, padx=padding[0], pady = padding[1])
+panel_sm.grid(row= 0, column = 1, padx=padding[0], pady = padding[1])
 
-panel_table = Label(log_frame, image = img1)
-panel_table.pack(fill = BOTH, expand = True)
+panel_table = Label(log_frame, image = img_tbl)
+#panel_table.pack(fill = BOTH, expand = True)
+panel_table.grid(row= 0, column = 0,padx=50)
 
 iteration_argument = StringVar()
 loop_argument = StringVar()
 def draw_updated():
-    img1 = ImageTk.PhotoImage(Image.open('Resources/table.png').resize((img_size, img_size), Image.ANTIALIAS))
-    img2 = ImageTk.PhotoImage(Image.open('Resources/bar_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-    img3 = ImageTk.PhotoImage(Image.open('Resources/msgq_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-    img4 = ImageTk.PhotoImage(Image.open('Resources/xd_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+    img_tbl = ImageTk.PhotoImage(Image.open('Resources/table.png').resize((img_size, img_size), Image.ANTIALIAS))
+    img_sm = ImageTk.PhotoImage(Image.open('Resources/shared_memory_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+    img_mq = ImageTk.PhotoImage(Image.open('Resources/msgq_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+    img_sem = ImageTk.PhotoImage(Image.open('Resources/semaphore_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
 
 
-    panel_xd = Label(matplot_frame, image = img4)
-    panel_hist = Label(matplot_frame, image = img3)
-    panel_bar = Label(matplot_frame, image = img2)
-    panel_xd.grid(row= 0, column = 2, padx=am, pady = 50)
-    panel_hist.grid(row= 0, column = 0, padx=am, pady = am)
-    panel_bar.grid(row= 0, column = 1, padx=am, pady = am)
-    panel_xd.image = img4
-    panel_hist.image = img3
-    panel_bar.image = img2
+    panel_table = Label(log_frame, image = img_tbl)
+    panel_sem = Label(matplot_frame, image = img_sem)
+    panel_mq = Label(matplot_frame, image = img_mq)
+    panel_sm = Label(matplot_frame, image = img_sm)
+    panel_mq.grid(row= 0, column = 0, padx=padding[0], pady = padding[1])
+    panel_sm.grid(row= 0, column = 1, padx=padding[0], pady = padding[1])
+    panel_sem.grid(row= 0, column = 2, padx=padding[0], pady = padding[1])
+    panel_table.grid(row=0,column=0)
+    panel_sem.image = img_sem
+    panel_mq.image = img_mq
+    panel_sm.image = img_sm
+    panel_table.image=img_tbl
 
-    panel_table = Label(log_frame, image = img1)
-    panel_table.pack(fill = BOTH)
-    panel_table.image=img1
-    # if(panel_xd != None):
-    #     panel_xd.image =img4
-    # if(panel_hist != None):
-    #     panel_hist.image=img2
-    # if(panel_bar != None):
-    #     panel_bar.image= img3
     count = 0
-    log_frame.update()
     matplot_frame.update()
+    log_frame.update()
 
 
 
@@ -285,15 +295,15 @@ def init():
     print(canvas['width'])
 
     root.bind('<Configure>',change_size_callback)
-    root.photo1 = ImageTk.PhotoImage(Image.open('Resources/bar_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+    root.photo1 = ImageTk.PhotoImage(Image.open('Resources/shared_memory_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
     root.photo2 = ImageTk.PhotoImage(Image.open('Resources/msgq_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
-    root.photo3 = ImageTk.PhotoImage(Image.open('Resources/xd_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
+    root.photo3 = ImageTk.PhotoImage(Image.open('Resources/semaphore_graph.png').resize((img_size, img_size), Image.ANTIALIAS))
     #root.bind('<Return>',change_image_callback)
     picker_w = 0.6; picker_h = 1;
     ypos = (c_height - (c_height / 3)) * picker_h
     picker_frame.place(width = c_width * picker_w, relheight=picker_h-0.1, y = ypos )
     matplot_frame.place(relwidth = 1.0 , height= ypos, x=0)
-    log_frame.place(relwidth = 1.0 - picker_w, height =c_height - ypos, y = ypos, x= c_width * picker_w)
+    log_frame.place(width = c_width - c_width * picker_w, relheight=picker_h-0.1, y = ypos, x= c_width * picker_w)
     root.update()
     #Label(log_frame, text="Log", bg="lightgray", font=(font_type, font_size)).pack()
     update_graph('echo starting')
@@ -394,8 +404,6 @@ def update_graph(command):
     sub_p(command)
     create_graph()
     create_table()
-    #sub_p('sudo python3 create_graph.py')   
-    #sub_p('sudo python3 bell_curve.py')   
     draw_updated()
     canvas.update()
     
@@ -406,15 +414,15 @@ error.grid(row=11,column=2, columnspan = 10, rowspan=2, sticky='w')
 def execute():
     msg.set("")
     f = 0
+    failed = False
     try:
         iteration_count = int(iteration_argument.get())
         if(iteration_count <= 0):
             f+=1
             msg.set('Please enter a valid positive \ninteger for iteration')
-            iteration_count = 100
-        #msg.set('nips')
+            failed = True
     except:
-        iteration_count = 100 #default
+        failed = True #default
         msg.set('Please enter a valid positive \ninteger for iteration')
         f += 1
     try:
@@ -423,36 +431,37 @@ def execute():
             msg.set('Please enter a valid positive \ninteger for loop')
             if(f == 1):
                 msg.set('Please enter a valid positive \ninteger for iteration and loop')
-            loop_count = 1
+            failed = True
         
 
     except:
         msg.set('Please enter a valid positive \ninteger for the loop')
         if(f == 1):
             msg.set('Please enter a valid positive \ninteger for iteration and loop')
-        loop_count = 1 #default
+        failed = True #default
 
-    #Native POSIX IPCs
-    if(my_map['native']['posix']['semaphore'].get()): update_graph('sudo ' +  driver + ' 5 0 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['native']['posix']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 6 0 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['native']['posix']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 7 0 ' + str(iteration_count) + ' ' + str(loop_count))
+    if(not failed):
+        #Native POSIX IPCs
+        if(my_map['native']['posix']['semaphore'].get()): update_graph('sudo ' +  driver + ' 5 0 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['native']['posix']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 6 0 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['native']['posix']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 7 0 ' + str(iteration_count) + ' ' + str(loop_count))
 
-    #Native SysV IPCs
-    if(my_map['native']['sysv']['semaphore'].get()): update_graph('sudo ' +  driver + ' 9 0 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['native']['sysv']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 10 0 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['native']['sysv']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 11 0 ' + str(iteration_count) + ' ' + str(loop_count))
+        #Native SysV IPCs
+        if(my_map['native']['sysv']['semaphore'].get()): update_graph('sudo ' +  driver + ' 9 0 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['native']['sysv']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 10 0 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['native']['sysv']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 11 0 ' + str(iteration_count) + ' ' + str(loop_count))
 
-    #Namespace POSIX IPCs
-    if(my_map['namespace']['posix']['semaphore'].get()): update_graph('sudo ' +  driver + ' 5 1 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['namespace']['posix']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 6 1 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['namespace']['posix']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 7 1 ' + str(iteration_count) + ' ' + str(loop_count))
+        #Namespace POSIX IPCs
+        if(my_map['namespace']['posix']['semaphore'].get()): update_graph('sudo ' +  driver + ' 5 1 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['namespace']['posix']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 6 1 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['namespace']['posix']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 7 1 ' + str(iteration_count) + ' ' + str(loop_count))
 
-    #Namespace SysV IPCs
-    if(my_map['namespace']['sysv']['semaphore'].get()): update_graph('sudo ' +  driver + ' 9 1 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['namespace']['sysv']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 10 1 ' + str(iteration_count) + ' ' + str(loop_count))
-    if(my_map['namespace']['sysv']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 11 1 ' + str(iteration_count) + ' ' + str(loop_count))
+        #Namespace SysV IPCs
+        if(my_map['namespace']['sysv']['semaphore'].get()): update_graph('sudo ' +  driver + ' 9 1 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['namespace']['sysv']['messagequeue'].get()): update_graph('sudo ' +  driver + ' 10 1 ' + str(iteration_count) + ' ' + str(loop_count))
+        if(my_map['namespace']['sysv']['sharedmemory'].get()): update_graph('sudo ' +  driver + ' 11 1 ' + str(iteration_count) + ' ' + str(loop_count))
 
-    update_log()
+        update_log()
 
 def update_log():
     #label = Label(log_frame, text="Native", bg="lightgrey").pack()
